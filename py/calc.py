43fec4756d51d1f1e0848c92a39b523e338162fe
SPS Vision <steven-ps@hotmail.com>
2018-09-05 14:44:39 -0700

    Update calc.py

--- py/calc.py
+++ py/calc.py
@@ -1,11 +1,3 @@
-'''rapid = RapidConnect("default-application_59bbda2be4b0b0cacf7c7995",
-                     "db4efe57-20a5-4f2d-966c-b292a14a8695")
-
-result = rapid.call('YahooWeatherAPI', 'getWeatherForecast', {'location': city})
-
-desc = result["query"]["results"]["channel"]["item"]["condition"]["text"]
-temp = result["query"]["results"]["channel"]["item"]["condition"]["temp"]
-'''
 from __future__ import print_function
 import sys
 
